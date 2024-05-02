ba205d652fc04f16653e7d9e9796091c68537895
rohankumarvg <71186517+rohankumarvg@users.noreply.github.com>
2024-05-02 14:29:06 +0530

    Added UI

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 let choices = ['rock', 'paper', 'scissors'];
 
-function askForInput() {
-  return prompt('Choose your weapon- Rock, Paper or Scissors').toLowerCase();
-}
-
-function getHumanChoice(userInput) {
-  switch (userInput) {
-    case 'rock':
-      return 'rock';
-    case 'paper':
-      return 'paper';
-    case 'scissors':
-      return 'scissors';
-    default:
-      alert('Insert a valid text input');
-      return askForInput();
-  }
-}
-
 function getComputerChoice(max) {
   let n = Math.floor(Math.random() * max);
   return choices[n];
@@ -25,74 +7,54 @@ function getComputerChoice(max) {
 
 let humanScore = 0;
 let computerScore = 0;
-let rounds = 0;
 
-function playRound() {
-  let humanChoice = getHumanChoice(askForInput());
-  let computerChoice = getComputerChoice(3).toLowerCase();
+const resultsDiv = document.getElementById('results');
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice(3).toLowerCase();
 
   if (
-    (humanChoice === 'rock' && computerChoice === 'paper') ||
-    (humanChoice === 'paper' && computerChoice === 'scissors') ||
-    (humanChoice === 'scissors' && computerChoice === 'rock')
+    (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
     computerScore++;
-    rounds++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    console.log(
-      `User score: ${humanScore}, Computer Score: ${computerScore} Current round: ${rounds}`
-    );
+    resultsDiv.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+    updateScore();
   } else if (
-    (humanChoice === 'rock' && computerChoice === 'scissors') ||
-    (humanChoice === 'paper' && computerChoice === 'rock') ||
-    (humanChoice === 'scissors' && computerChoice === 'paper')
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    resultsDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     humanScore++;
-    rounds++;
-    console.log(
-      `User score: ${humanScore}, Computer Score: ${computerScore} Current round: ${rounds}`
-    );
+    updateScore();
   } else {
-    console.log(`That was a tie. Play again.`);
-    console.log(
-      `User score: ${humanScore}, Computer Score: ${computerScore} Current round: ${rounds}`
-    );
+    resultsDiv.textContent = `That was a tie. Play again.`;
+    updateScore();
   }
 
-  if (rounds === 5) {
+  if (humanScore === 5 || computerScore === 5) {
     announceWinner();
-    setTimeout(resetGame, 5000);
-  } else {
-    playGame();
   }
 }
 
-function resetGame() {
-  let userInput = prompt(`Do you want to play again?`).toLowerCase();
-  if (userInput === 'yes') {
-    humanScore = 0;
-    computerScore = 0;
-    rounds = 0;
-    playGame();
-  } else {
-    humanScore = 0;
-    computerScore = 0;
-    rounds = 0;
-    console.log(`Thanks for playing!`);
-  }
+function updateScore() {
+  resultsDiv.textContent += `\nUser score: ${humanScore}, Computer Score: ${computerScore}`;
 }
 
 function announceWinner() {
-  if (humanScore > computerScore) {
-    console.log(`Congratulations! You win`);
-  } else {
-    console.log(`Sorry... You lost.`);
+  if (humanScore === 5) {
+    resultsDiv.textContent = 'Congratulations! You win';
+  } else if (computerScore === 5) {
+    resultsDiv.textContent = 'Sorry... You lost.';
   }
 }
 
-function playGame() {
-  playRound();
-}
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
 
-playGame();
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
